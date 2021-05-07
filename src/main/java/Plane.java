c62d58484d19ff034430b08becfe81cc53454b5c
Pete <peterwbrodie@gmail.com>
2021-05-07 13:50:56 +0100

    Plane class complete

---
+++ src/main/java/Plane.java
@@ -0,0 +1,29 @@
+public enum Plane {
+
+    EASYJET(100, 1000),
+    JETSTAR(200, 2000),
+    QANTAS (300, 3000),
+    ETIHAD (400, 4000),
+    QATAR (500, 5000);
+
+    private final int capacity;
+    private final int totalWeight;
+
+    Plane(int capacity, int totalWeight) {
+        this.capacity = capacity;
+        this.totalWeight = totalWeight;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getTotalWeight() {
+        return totalWeight;
+    }
+
+}
+
+
+
+    }
