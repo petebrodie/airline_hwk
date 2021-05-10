e0d577f4ad6691aad1eb9e2e7ed58219d0363dd2
Pete <peterwbrodie@gmail.com>
2021-05-10 08:31:30 +0100

    Flight Class

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+
+
 public class Flight {
+
+    private ArrayList<Passenger> passengers;
+    private Plane plane;
+    private String FlightNumber;
+    private String Destination;
+    private String DepartureAirport;
+    private String DepartureTime;
+
+
+    public Flight(Plane plane, String flightNumber, String destination, String departureAirport, String departureTime) {
+        this.passengers = new ArrayList<Passenger>();
+        this.FlightNumber = flightNumber;
+        this.Destination = destination;
+        this.DepartureAirport = departureAirport;
+        this.DepartureTime = departureTime;
+
+
+
+    }
+
+
 }
+
+
--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FlightTest {
+
+    Passenger passenger;
+
+    @Before
+    public void setUp() {
+        passenger = new Passenger("Pete");
+
+
+    }
+
+    @Test
+    public void getPassengerName() {
+        assertEquals();
+    }
+
+
+
 }
